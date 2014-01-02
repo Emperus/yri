ac67919c7bfcf3e7294256b0cd4aa86e1fb60cdf
emperus <emperus2000@hotmail.com>
2014-01-02 17:25:33 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yri::Application.config.secret_key_base = 'eae0fcc44c9ad1d68de94cf9c7719fffb2af6251994d8865445345896f2114f30c6764c9f68fe280977de26bd8f9ed96e8debf58cef2aa94faf6b721bd0c0dd3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yri::Application.config.session_store :cookie_store, key: '_yri_session'
